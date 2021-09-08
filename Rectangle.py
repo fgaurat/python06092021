f61cc145dc7bff6a68d85311aa0945372ca9c1eb
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-08 16:17:16 +0200

    ch09 et Rectangle

--- Rectangle.py
+++ Rectangle.py
@@ -3,27 +3,37 @@
 
 class Rectangle:
     """Rectangle class"""
-    cpt = 0
+    
+    _cpt = 0
+    
     def __init__(self,*,longueur=0,largeur=0) -> None: # double under init , dunder init
         print("def __init__(self)")
         self._longueur=longueur # définition de la propriété longueur
         self._largeur=largeur # définition de la propriété largeur
-        Rectangle.cpt+=1
+        Rectangle._cpt+=1
 
-    def get_surface(self):
+    @property
+    def surface(self):
         return self._longueur*self._largeur
 
-    def get_longueur(self):
+    @property
+    def longueur(self):
         return self._longueur
-    
-    def set_longueur(self,longueur):
+
+    @longueur.setter
+    def longueur(self,longueur):
         self._longueur = longueur
 
-    def get_largeur(self):
+    @property
+    def largeur(self):
         return self._largeur
-    
-    def set_largeur(self,largeur):
+
+    @largeur.setter
+    def largeur(self,largeur):
         self._largeur = largeur
 
     def __str__(self) -> str:
-        return f"{__class__} longueur : {self._longueur} largeur : {self._largeur}"
+        return f"{__class__.__name__} longueur : {self._longueur} largeur : {self._largeur}"
+    
+    def get_cpt():
+        return Rectangle._cpt
--- ch09.py
+++ ch09.py
@@ -3,16 +3,21 @@ from Rectangle import Rectangle
 
 
 def main():
-    r =Rectangle(longueur = 2,largeur = 3)
+    r = Rectangle(longueur = 2,largeur = 3)
+    r2 = Rectangle(longueur = 2,largeur = 3)
     print(r._longueur,r._largeur)
-    surface = r.get_surface() # 24
+    surface = r.surface # 24
     print(surface)
-    longueur = r.get_longueur() # 12
-    print(longueur)
-    largeur = r.get_largeur() # 2
-    print(largeur)
+    # r.set_longueur(56)
+    print(r.longueur)
+    r.longueur = 56
+
+    # longueur = r.get_longueur() # 12
+    # print(longueur)
+    # largeur = r.get_largeur() # 2
+    # print(largeur)
     print(r)
-    print("Rectangle.cpt",Rectangle.cpt)
+    print("Rectangle.cpt",Rectangle.get_cpt())
 
 
 if __name__ == '__main__':
