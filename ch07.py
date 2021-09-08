f50ab697b242a35b3da288e78225bf991a5733e7
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-08 10:02:30 +0200

    ch07

--- ch07.py
+++ ch07.py
@@ -2,8 +2,13 @@ def main():
     name="GAURAT"
     first_name="Fred"
 
-    say_hello = "Hello "+name+" "+first_name
-    print(say_hello)
+    say_hello_1 = "Hello "+name+" "+first_name
+    say_hello_2 = "Hello {0} {1}".format(name,first_name)
+    # s = r"c:\newdir" #raw
+    say_hello_3 = f"Hello {name} {first_name}"
+    print(say_hello_1)
+    print(say_hello_2)
+    print(say_hello_3)
 
 
 if __name__ == '__main__':
