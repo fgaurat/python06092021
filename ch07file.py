ed30ad3d754b6847cbe44aea6b5362d2c65de392
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-08 11:48:15 +0200

    ch07file

--- ch07file.py
+++ ch07file.py
@@ -6,13 +6,28 @@ def main():
 
     # mode a : append
     # mode w : write
+    # mode r : read (default)
     # with open("lefichier.txt","a") as f:
     #     f.write("Bonjour\n")
     #     print("Bonjour",file=f)
     #     print(f.closed)
     # print(f.closed)
 
+    # with open("lefichier.txt") as f:
+    # with open("lefichier.txt","r") as f:
+    #     # lines = f.readlines()
+    #     # print(lines)
+    #     for line in f:
+    #         # print(line.strip())
+    #         print(line,end="")
 
+
+    users = [
+        {"login":"admin","password":"12345"},
+        {"login":"user1","password":"34989"},
+        {"login":"user2","password":"345"},
+        {"login":"user3","password":"123"},
+    ]
 
 if __name__ == '__main__':
     main()
