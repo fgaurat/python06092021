29786ad8fa60006764cf43b1175c019ce97ee1bc
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-08 10:21:35 +0200

    ch07

--- ch07.py
+++ ch07.py
@@ -5,11 +5,15 @@ def main():
     say_hello_1 = "Hello "+name+" "+first_name
     say_hello_2 = "Hello {0} {1}".format(name,first_name)
     # s = r"c:\newdir" #raw
-    say_hello_3 = f"Hello {name} {first_name}"
+    say_hello_3 = f"Hello {name[0]} {first_name[0]}"
     print(say_hello_1)
     print(say_hello_2)
     print(say_hello_3)
 
+    a= 1
+    b= 3
+    c = 0.339
+    print(f"Valeur de c : {c:.2%}")
 
 if __name__ == '__main__':
     main()
