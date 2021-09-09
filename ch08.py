1c86674dd0a353f33e11dde703d4c9ea16ca5c0b
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-09 11:34:09 +0200

    Cercle et Interface

--- ch08.py
+++ ch08.py
@@ -1,8 +1,16 @@
+import sys
 def main():
-    a=0
-    b=2
-    c= b/a
-    print(c)
+    try:
+        a = int(input("Please enter a number: "))
+        b=2
+        c= b/a # < ici !
+        print(c)
+    except ZeroDivisionError as e:
+        # print("Erreur !ZeroDivisionError",file=sys.stderr)
+        print("ZeroDivisionError",e)
+    except Exception as e:
+        # print("Erreur !ZeroDivisionError",file=sys.stderr)
+        print("Exception",e)
 
 
 if __name__ == '__main__':
