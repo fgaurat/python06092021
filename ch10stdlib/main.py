236a0ee974675fa7a2baa85451740503d6525cb7
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-09 16:07:53 +0200

    ch10 venv et requests

--- ch10stdlib/main.py
+++ ch10stdlib/main.py
@@ -1,6 +1,7 @@
 import requests
+import glob
 
-def main():
+def main_requests():
 
     for i in range(1,5):
         file_name = f"apache_logs_{i:02}.log"
@@ -10,6 +11,9 @@ def main():
         with open(file_name,"w") as f:
             print(r.text,file=f)
 
+def main():
+    log_files = glob.glob('*.log')
+    print(log_files)
 
 if __name__ == '__main__':
     main()
