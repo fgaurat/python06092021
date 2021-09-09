fc48deb22dd5646c073daf3df2cef0d6b1850870
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-09 15:37:20 +0200

    ch10 venv et requests

--- ch10stdlib/main.py
+++ ch10stdlib/main.py
@@ -1,9 +1,15 @@
 import requests
 
 def main():
-    url = "http://logs.eolem.com/apache_logs_01.log" 
-    r = requests.get(url)
-    print(r.text)
+
+    for i in range(1,5):
+        file_name = f"apache_logs_{i:02}.log"
+        url = f"http://logs.eolem.com/{file_name}"
+        print(url)
+        r = requests.get(url)
+        with open(file_name,"w") as f:
+            print(r.text,file=f)
+
 
 if __name__ == '__main__':
     main()
