df3916324e9f65c5c27afbf3c7a450b51298faef
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-10 15:08:20 +0200

    tpbdd json et sqlite ORM,DAO et Tkinter (window)

--- tpbdd/win_main.py
+++ tpbdd/win_main.py
@@ -5,16 +5,15 @@ import sqlite3
 import configparser
 import argparse
 
+
 class Application(tk.Frame):
 
-    def __init__(self, master=None,db_file=None):
+    def __init__(self, master=None, db_file=None):
         super().__init__(master)
         self.master = master
         self.pack()
-        self.create_widgets()
         self._dao = TodoDAO(db_file=db_file)
-
-
+        self.create_widgets()
 
     def create_widgets(self):
         # self.hi_there = tk.Button(self)
@@ -25,31 +24,35 @@ class Application(tk.Frame):
         # self.quit = tk.Button(self, text="QUIT", fg="red",command=self.master.destroy)
         # self.quit.pack(side="bottom")
         self.todo_table = ttk.Treeview(self)
-        self.todo_table['columns'] = ('id','title','completed')
+        self.todo_table['columns'] = ('id', 'title', 'completed')
         self.todo_table['show'] = 'headings'
         # conf
-        self.todo_table.column('id',anchor=tk.CENTER,width=80)
-        self.todo_table.column('title',anchor=tk.CENTER,width=80)
-        self.todo_table.column('completed',anchor=tk.CENTER,width=80)
+        self.todo_table.column('id', anchor=tk.CENTER, width=80)
+        self.todo_table.column('title', anchor=tk.CENTER, width=80)
+        self.todo_table.column('completed', anchor=tk.CENTER, width=80)
         # header
         self.todo_table.heading('id', text='id', anchor=tk.CENTER)
         self.todo_table.heading('title', text='title', anchor=tk.CENTER)
-        self.todo_table.heading('completed', text='completed', anchor=tk.CENTER)
+        self.todo_table.heading(
+            'completed', text='completed', anchor=tk.CENTER)
 
         mes_todos = self._dao.find_all()
 
         for todo in mes_todos:
-            self.todo_table.insert(parent='', index=todo.id, iid=todo.id, text='', values=(todo.id,todo.title,todo.completed))
-
+            self.todo_table.insert(parent='', index=todo.id, iid=todo.id, text='', values=(
+                todo.id, todo.title, todo.completed))
 
         self.todo_table.pack(fill="both")
 
     def say_hi(self):
         print("hi there, everyone!")
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Insert todos from web in sqlite')
-    parser.add_argument('config',help='configuration file',type=str,default='config.ini')
+    parser = argparse.ArgumentParser(
+        description='Insert todos from web in sqlite')
+    parser.add_argument('config', help='configuration file',
+                        type=str, default='config.ini')
     args = parser.parse_args()
     config = configparser.ConfigParser()
     config.read(args.config)
@@ -57,8 +60,8 @@ def main():
     db_file = config['BDD']['name']
 
     root = tk.Tk()
-    app = Application(master=root,db_file=db_file)
-    root.geometry('800x600+500+200')
+    app = Application(master=root, db_file=db_file)
+    # root.geometry('800x600+500+200')
     app.mainloop()
 
 
