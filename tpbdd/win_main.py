fb815afe8b7a0c2acb15030382bbd5d3da60d192
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-10 15:00:40 +0200

    tpbdd json et sqlite ORM,DAO et Tkinter (window)

--- tpbdd/win_main.py
+++ tpbdd/win_main.py
@@ -18,6 +18,21 @@ class Application(tk.Frame):
         # self.quit = tk.Button(self, text="QUIT", fg="red",command=self.master.destroy)
         # self.quit.pack(side="bottom")
         self.todo_table = ttk.Treeview(self)
+        self.todo_table['columns'] = ('id','title','completed')
+        self.todo_table['show'] = 'headings'
+        # conf
+        self.todo_table.column('id',anchor=tk.CENTER,width=80)
+        self.todo_table.column('title',anchor=tk.CENTER,width=80)
+        self.todo_table.column('completed',anchor=tk.CENTER,width=80)
+        # header
+        self.todo_table.heading('id', text='id', anchor=tk.CENTER)
+        self.todo_table.heading('title', text='title', anchor=tk.CENTER)
+        self.todo_table.heading('completed', text='completed', anchor=tk.CENTER)
+
+        for i in range(5):
+            self.todo_table.insert(parent='', index=i, iid=i, text='', values=("Col 1","Col 1","Col 1"))
+
+
         self.todo_table.pack(fill="both")
 
     def say_hi(self):
