c4d7c9ac664ecf65ee06687baebf8e1439acb703
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-10 14:04:56 +0200

    tpbdd json et sqlite ORM et DAO

--- tpbdd/TodoDAO.py
+++ tpbdd/TodoDAO.py
@@ -1,5 +1,20 @@
-
-
+import sqlite3
+from Todo import Todo
 
 class TodoDAO:
-    pass
+    
+    def __init__(self,*,db_file) -> None:
+        self._con = sqlite3.connect(db_file)
+
+    def find_all(self):
+        cur = self._con.cursor()
+        sql = r"SELECT id,title,completed FROM todos"
+        result_set = cur.execute(sql)
+        
+        # result = [Todo(id=row[0],title=row[1],completed=row[2]) for row in result_set]
+        result = []
+        for row in result_set:
+            t = Todo(id=row[0],title=row[1],completed=row[2])
+            result.append(t)
+        
+        return result
