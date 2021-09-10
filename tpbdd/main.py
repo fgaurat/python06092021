2a6bed41a9348c679bb58ec1c49fb3addbd40672
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-10 10:52:21 +0200

    tpbdd json et sqlite

--- tpbdd/main.py
+++ tpbdd/main.py
@@ -1,10 +1,16 @@
 import requests
+from pprint import pprint
 
 def main():
     url = "https://jsonplaceholder.typicode.com/todos"
     resp = requests.get(url)
     todos = resp.json() 
-    print(todos)
+    # print(todos)
+    # pprint(todos)
+    # print(todos[0])
+
+    for todo in todos:
+        print(todo['title'],todo['completed'])
 
 
 if __name__ == '__main__':
